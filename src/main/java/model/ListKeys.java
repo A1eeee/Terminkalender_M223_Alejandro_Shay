1f9ce06b2953fb3df6cf37f424cd58ce85435cad
Ale <alejandro.wenzler@gmail.com>
2023-05-09 16:16:10 +0200

    ListKeys

--- src/main/java/model/ListKeys.java
+++ src/main/java/model/ListKeys.java
@@ -1,4 +1,67 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+
 public class ListKeys {
+
+    private ArrayList<String> keyList;
+
+    public ListKeys(){
+        this.keyList = new ArrayList<>();
+    }
+
+    private String generateKey(){
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        return generatedString;
+    }
+
+    private boolean checkKey(String keyL)
+    {
+        Iterator<String> it = keyList.iterator();
+
+        while(it.hasNext())
+        {
+            String key = it.next();
+            if(key.equals(keyL))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String keyadd(){
+        boolean temp = false;
+
+        while (!temp) {
+            String key = generateKey();
+
+            if (checkKey(key) == true) {
+                keyList.add(key);
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<String> getKeyList() {
+        return keyList;
+    }
+
+    public void setKeyList(ArrayList<String> keyList) {
+        this.keyList = keyList;
+    }
 }
